73292be6b373e68fc8e0215613782770f898d448
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2023-12-21 23:02:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const links = document.querySelectorAll(".Question");
+
+links.forEach(function(links){
+    links.addEventListener('click',function(l){
+        const index = l.currentTarget;
+
+        
+
+    });
+});
