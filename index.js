7b3beda1115ad178884c809f6c5e079d91caeec6
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2023-12-22 16:05:36 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+const questions = document.querySelectorAll('.que');
 
-const links = document.querySelectorAll(".Question");
-
-links.forEach(function(links){
-    links.addEventListener('click',function(l){
-        const index = l.currentTarget;
-
+questions.forEach(ques => {
+    ques.addEventListener('click', function(){
+        this.classList.toggle('active')
+        let answer = this.nextElementSibling;
+        const plusIcon = this.querySelector('.plus');
+        const minusIcon = this.querySelector('.minus');
         
-
+        if (answer.style.maxHeight) {
+            answer.style.maxHeight = null;
+            plusIcon.style.display = 'block';
+            minusIcon.style.display = 'none';
+        } else {
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+            minusIcon.style.display = 'block';
+            plusIcon.style.display = 'none';
+        }
     });
 });
